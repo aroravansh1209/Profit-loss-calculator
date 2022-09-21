1f8644f86e49e855e88d9aa70fc6773490d2c1dd
aroravansh1209 <109975931+aroravansh1209@users.noreply.github.com>
2022-09-21 16:41:09 +0530

    fixed

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const purchasePrice = document.querySelector("#purchase-price")
-const quatityPurchased = document.querySelector("#quantity")
-const marketPrice = document.querySelector("#current-price")
-const calculateButton = document.querySelector("#calculate")
-const outputEl = document.querySelector("#output")
+const purchasePrice = document.querySelector("#purchase-price");
+const quatityPurchased = document.querySelector("#quantity");
+const marketPrice = document.querySelector("#current-price");
+const calculateButton = document.querySelector("#calculate");
+const outputEl = document.querySelector("#output");
 
-function calculateProfitAndLoss(){
-    if(purchasePrice.value > marketPrice.value){
-        var loss = (purchasePrice.value - marketPrice.value) * quatityPurchased.value
-        var lossPercentage = (loss/purchasePrice.value)*100
-        outputEl.innerText = (`The loss is ${loss} and the percentage of loss is ${lossPercentage}`)
-    }else if(purchasePrice.value < marketPrice.value){
-        var profit = (marketPrice.value - purchasePrice.value) * quatityPurchased.value
-        var profitPercentage = (profit/purchasePrice.value)*100
-        outputEl.innerText = (`The profit is ${profit} and the percentage of profit is ${profitPercentage}`)
-    }else{
-        outputEl.innerText = ("There is no profit no loss yet.")
-    }
+function calculateProfitAndLoss() {
+  if (Number(purchasePrice.value) > Number(marketPrice.value)) {
+    var loss =
+      (Number(purchasePrice.value) - Number(marketPrice.value)) *
+      quatityPurchased.value;
+    var lossPercentage = (loss / Number(purchasePrice.value)) * 100;
+    outputEl.innerText = `The loss is ${loss} and the percentage of loss is ${lossPercentage}`;
+  } else if (Number(purchasePrice.value) < Number(marketPrice.value)) {
+    var profit =
+      (Number(marketPrice.value) - Number(purchasePrice.value)) *
+      quatityPurchased.value;
+    var profitPercentage = (profit / Number(purchasePrice.value)) * 100;
+    outputEl.innerText = `The profit is ${profit} and the percentage of profit is ${profitPercentage}`;
+  } else {
+    outputEl.innerText = "There is no profit no loss yet.";
+  }
 }
 
-calculateButton.addEventListener('click',calculateProfitAndLoss)
+calculateButton.addEventListener("click", calculateProfitAndLoss);
