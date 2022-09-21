63831a20f3aa495308ed5f633c8a986a75ccfc18
aroravansh1209 <109975931+aroravansh1209@users.noreply.github.com>
2022-09-21 17:16:18 +0530

    updated formulae

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ const calculateButton = document.querySelector("#calculate");
 const outputEl = document.querySelector("#output");
 
 function calculateProfitAndLoss() {
+  let calculation = purchasePrice.value * quatityPurchased.value;
+
   if (Number(purchasePrice.value) > Number(marketPrice.value)) {
     var loss =
       (Number(purchasePrice.value) - Number(marketPrice.value)) *
       quatityPurchased.value;
-    var lossPercentage = (loss / Number(purchasePrice.value)) * 100;
+    var lossPercentage = (loss / calculation) * 100;
     outputEl.innerText = `The loss is ${loss} and the percentage of loss is ${lossPercentage}`;
   } else if (Number(purchasePrice.value) < Number(marketPrice.value)) {
     var profit =
       (Number(marketPrice.value) - Number(purchasePrice.value)) *
       quatityPurchased.value;
-    var profitPercentage = (profit / Number(purchasePrice.value)) * 100;
+    var profitPercentage = (profit / calculation) * 100;
     outputEl.innerText = `The profit is ${profit} and the percentage of profit is ${profitPercentage}`;
   } else {
     outputEl.innerText = "There is no profit no loss yet.";
